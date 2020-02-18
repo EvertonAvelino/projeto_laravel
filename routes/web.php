7ff2ab80199deb6f4e8901a9730cab5932d9cfb2
EvertonAvelino <56169155+EvertonAvelino@users.noreply.github.com>
2020-02-18 14:52:22 -0300

    criando projeto laravel

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+| ------------------------------------------------- -------------------------
+| Rotas da Web
+| ------------------------------------------------- -------------------------
+|
+| Aqui é onde você pode registrar rotas da web para seu aplicativo. Estes
+| rotas são carregadas pelo RouteServiceProvider dentro de um grupo que
+| contém o grupo de middleware "web". Agora crie algo ótimo!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
